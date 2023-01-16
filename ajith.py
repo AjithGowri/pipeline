b4603b370231613178405c16e66d050bdcb48848
Ajith <ajithgowri111@gmail.com>
2023-01-16 09:37:35 +0530

    Jenkins connection

--- ajith.py
+++ ajith.py
@@ -1,5 +1,7 @@
 print('Hello')
 a=1000
 b=200
-print('The result is ',a+b)
+print('Addition is ',a+b)
+print('Subtraction is',a-b)
 print('Successfully created pipeline')
+print('Git + Github + Jenkins')
